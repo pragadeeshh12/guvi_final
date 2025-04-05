82fb859cbe585d2ed5b4ab31c814f7c68a07f5e6
Pragadeeshwar K <135110068+pragadeeshh12@users.noreply.github.com>
2025-04-05 12:17:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 docker build -t task2 .
 echo Hyyy
 docker login -u sridinesh -p dckr_pat_f4KWk7nulMpQ1t1qAmdhNivaF9A
+echo "dckr_pat_f4KWk7nulMpQ1t1qAmdhNivaF9A" | docker login -u "sridinesh" --password-stdin
 docker tag task2 sridinesh/final
 docker push sridinesh/final
 kubectl apply -f  deploy.yaml --validate=false
