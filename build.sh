2473a7a30213d537fd7800814e2ce2bd73aa1f8d
Pragadeeshwar K <135110068+pragadeeshh12@users.noreply.github.com>
2025-04-05 11:42:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build -t task2 .
 echo Hyyy
-docker login -u deepak102 -p deepak@2005
-docker tag task2 deepak102/devops
-docker push deepak102/devops
+docker login -u sridinesh -p dckr_pat_f4KWk7nulMpQ1t1qAmdhNivaF9A
+docker tag task2 sridinesh/final
+docker push sridinesh/final
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
